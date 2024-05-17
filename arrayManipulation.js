f50d415d0edf872c8c73ed53d1725b679f159768
bigslime911 <fdonkor024@st.ug.edu.gh>
2024-05-17 08:29:01 -0700

    Task two

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -7,3 +7,16 @@ function processArray(arr) {
   const numbers = [13, 7, 3, 6, 10];
   const processedArray = processArray(numbers);
   console.log(processedArray); 
+
+  // Task Two
+  function formatArrayStrings(stringArr, numberArr) {
+    return stringArr.map((str, index) => {
+      return numberArr[index] % 2 === 0 ? str.toUpperCase() : str.toLowerCase();
+    });
+  }
+
+const stringNames = ['Kwame', 'Orange', 'Hippopotamus', 'Dansoman', 'Awudome'];
+const numberArray = [1, 2, 3, 4, 5];
+const formattedStringArray = formatArrayStrings(stringNames, numberArray);
+
+console.log(formattedStringArray);  
